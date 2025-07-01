4ddda4eb0ebe6b82e0142b7dfff8a2768a73745f
Hampus Ram <hampus.ram@gmail.com>
2025-07-01 17:50:46 +0200

    make uid more unique

--- icalmaker.py
+++ icalmaker.py
@@ -8,16 +8,18 @@ def timetable_to_ical(timetables: list) -> Calendar:
     cal = Calendar()
 
     for timetable in timetables:
+        time_start = pytz.utc.normalize(timetable.time_start)
+        time_end = pytz.utc.normalize(timetable.time_end)
+
+        timestamp_start = time_start.timestamp()
+
         event = Event()
-        event.add("uid", timetable.id + "@scheduletocalendar")
+        event.add("uid", str(int(timestamp_start)) + "-" + timetable.id + "@stc")
         event.add("summary", timetable.name)
         event.add(
             "description",
             f"Class: {timetable.group}\nTeacher: {timetable.teacher}\nRoom: {timetable.room}",
         )
-
-        time_start = pytz.utc.normalize(timetable.time_start)
-        time_end = pytz.utc.normalize(timetable.time_end)
 
         event.add("dtstart", time_start)
         event.add("dtend", time_end)
